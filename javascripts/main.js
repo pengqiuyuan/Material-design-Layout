5a4148c00bd6c82feb3c32c76531b72f452f353a
pengqiuyuan <pengqiuyuanfj@gmail.com>
2014-10-14 16:35:12 +0800

    1、自动布局、瀑布流、isotope的使用

--- javascripts/main.js
+++ javascripts/main.js
@@ -62,6 +62,35 @@
         $('body, html').toggleClass("nav-open-right");
     });
 
+
+      /*
+       # =============================================================================
+       #   Isotope with Masonry
+       # =============================================================================
+       */
+      var $container2;
+      $alpha = $('#hidden-items');
+      $container2 = $('#social-container');
+      $(window).load(function() {
+          /*
+           # init isotope, then insert all items from hidden alpha
+           */
+
+          $container2.isotope({
+              itemSelector: '.item'
+          }).isotope('insert', $alpha.find('.item'));
+          return $("#load-more").html("Load more").find("i").hide();
+      });
+      $('#load-more').click(function() {
+          var item1, item2, item3, items, tmp;
+          items = $container2.find('.social-entry');
+          item1 = $(items[Math.floor(Math.random() * items.length)]).clone();
+          item2 = $(items[Math.floor(Math.random() * items.length)]).clone();
+          item3 = $(items[Math.floor(Math.random() * items.length)]).clone();
+          tmp = $().add(item1).add(item2).add(item3);
+          return $container2.isotope('insert', tmp);
+      });
+
   });
 
 }).call(this);
